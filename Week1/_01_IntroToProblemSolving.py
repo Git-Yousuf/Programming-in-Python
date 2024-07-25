b87cd2857b2b85244f87b15d7ea68409f52d2701
Git-Yousuf <mohammedyousuf432003@gmail.com>
2024-07-25 12:53:47 +0530

    Introduction to the course and problem solving. Completed on 25 July 2024 12:53 PM.

---
+++ Week1/_01_IntroToProblemSolving.py
@@ -0,0 +1,37 @@
+"""
+Problem:
+According to Wikipedia - A problem is a situation preventing something from being achieved.
+"""
+
+"""
+Different approach to solve a problem: 
+Solutions can be categorized into 2 types.
+
+1. Algorithmic Approach:
+It is a defined set of step by step procedures that gives the correct answer to a particular problem.
+
+2. Experimentation Approach:
+when there is no specific path for the solution, we need to experiment with a different set of solutions 
+to get the correct as well as efficient solutions. This is an Experimental Approach.
+
+Experimental Approach - IDEAL
+Identify -> Define -> Explore -> Action -> Look Back -> Identify.
+"""
+
+"""
+The Three (Important) W's:
+1. What is the Problem?
+2. What are the facts linked with the problem?
+3. What method to be taken to solve the problem?
+"""
+
+"""
+Types of Coding approach:
+1. Programming Language.
+2. Scripting Language.  
+"""
+
+"""
+Programming Language:
+communication medium between the human and the computer via which we can give a command to the computer is known as the programming language.
+"""
