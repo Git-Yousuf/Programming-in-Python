1f331c8c2c9727273a797d0c88ec4c182f888418
Git-Yousuf <mohammedyousuf432003@gmail.com>
2024-07-25 18:47:17 +0530

    Computer as a problem solving tool . completed at 25 July 2024 6:45 PM

---
+++ Week1/_02_ComputerAsPSTool.py
@@ -0,0 +1,35 @@
+"""
+How to solve a problem with the computer?
+1. Proper understanding and description of the problem.
+2. Convert problem to solution
+    - Knowledge of the problem
+    -Select best strategy to solve
+"""
+
+"""
+To solve a problem - Generally
+1. we have to understand or study the cause.
+2. List the set of solutions.
+3. Check which one is the best.
+4. Implement the solutions.
+5. Check the effectiveness.
+"""
+
+"""
+A program is set of instructions given to computer to solve a problem.
+"""
+
+"""
+When we use computer as a problem solving tool - it consists of following measures:
+1. Defining the problem.
+2. Analyzing the problem.
+    - What will be the input?
+    - What kind of output is needed?
+    - Any specific conditions.
+3. Writing Codes.
+4. Compilation and Execution.
+5. Debugging and Testing.
+6. Documentation.
+    - Technical Documentation - covers everything from problem analysis to the implementation details of the program.
+    - Manual Documentation - created for the end user of a program and it guides the user through the operations of the program.
+"""
