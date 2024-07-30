6e530fe416ecee87d5fb71937872ebcb464ceae8
Git-Yousuf <mohammedyousuf432003@gmail.com>
2024-07-30 11:06:15 +0530

    Completed Week 1

---
+++ Week1/_03_TypesOfErrorAndIntroToDebugging.py
@@ -0,0 +1,61 @@
+"""
+Various Steps involved in problem solving:
+1. Problem Analysis
+2. Algorithm Developement
+3. Flow Charting
+4. Program Coding
+5. Compilation and Execution 
+6. Debugging and Testing
+7. Documentation
+"""
+
+"""
+Error - Any kind of unexpected output.
+"""
+
+"""
+Three Main Categories of error:
+1. Compiler error ( Syntax Error ) - happen when the code is being built either by an interpreter or by an compiler which prevent your code from running.
+2. Runtime Error - happens when code runs but which make your program crash in some way.
+3. Logical Error - do not make your program crash but result in an incorrect behaviour.
+"""
+
+"""
+Very Common Runtime Error are:
+1. Null reference or Null Pointer Exception
+2. Array index out of bounds
+3. Running out of memory
+4. Floating Pointer Exception
+5. Segmentataion Fault
+"""
+
+"""
+Very Common Logic errors are:
+1. using == instead of the equals method
+2. infinite loops
+3. misunderstanding of operators
+4. starting or ending at the wrong index of an array
+5. Misplaced parentheses
+"""
+
+"""
+Debugging Techniques:
+1. Reactive Techniques - print debugging
+2. Pre emptive Techniques - Assertions, Logging
+3. Debugger
+"""
+
+"""
+Documentation:
+- Textual information within a program
+- Helps to convey the meaning of the program to human readers who need to understand, modify or debug the program.
+"""
+
+"""
+Forms of documentation:
+1. Inherent Documentation.
+2. Inline Documentation.
+3. Header comments.
+4. External Documentation.
+5. User Manual.
+"""
