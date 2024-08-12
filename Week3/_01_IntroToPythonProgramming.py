963722429faed00ddc58cf6920d6cfc17a9e3632
Git-Yousuf <mohammedyousuf432003@gmail.com>
2024-08-12 23:24:36 +0530

    Completed till 2 videos

---
+++ Week3/_01_IntroToPythonProgramming.py
@@ -0,0 +1,20 @@
+"""
+
+IDE - Integrated Development Environment
+
+Some of the most popular IDEs are:
+1. Spider
+2. pycharms
+3. Sublime Text
+4. IDLE - Integrated Development and Learning Environment
+5. Visual Studio Code
+6. Komodo
+"""
+
+"""
+Python is a general purpose programming language which can be performed for variety of task such as web develpoment, software develpoment, scripting, data analysis, artificial intelligence, machine learning etc...  
+"""
+
+"""
+Syntax is nothing but a grammer to user language.
+"""
---
+++ Week3/_02_SyntaxVariableDatatype.py
@@ -0,0 +1,5 @@
+"""
+Syntax - refers to the rules that define the structure of a language.
+In Computer Programming, syntax is the rules that control the structure of the symbols, punctuation and words of a programming language.
+"""
+
